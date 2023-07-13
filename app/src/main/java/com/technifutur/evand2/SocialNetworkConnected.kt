2713b3095b399424fefa8aca55799c55312ecf11
Duc <luuduc34@hotmail.com>
2023-07-13 10:08:17 +0200

    EVAND2 - signup bloggies et social network connected activity

---
+++ app/src/main/java/com/technifutur/evand2/SocialNetworkConnected.kt
@@ -0,0 +1,11 @@
+package com.technifutur.evand2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SocialNetworkConnected : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_social_network_connected)
+    }
+}
