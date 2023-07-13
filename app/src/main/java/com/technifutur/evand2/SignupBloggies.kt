58a7d99762bf9f4ef1ff4c045b44eeffa77d2eda
Duc <luuduc34@hotmail.com>
2023-07-13 09:35:14 +0200

    EVAND2 - twitter login activity

---
+++ app/src/main/java/com/technifutur/evand2/SignupBloggies.kt
@@ -0,0 +1,11 @@
+package com.technifutur.evand2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupBloggies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_bloggies)
+    }
+}
---
+++ app/src/main/java/com/technifutur/evand2/TwitterLogin.kt
@@ -0,0 +1,11 @@
+package com.technifutur.evand2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TwitterLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twitter_login)
+    }
+}
