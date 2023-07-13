ce412aab52944a9526a8b348a6c0332b7c8f4491
Duc <luuduc34@hotmail.com>
2023-07-13 09:52:10 +0200

    EVAND2 - facebook et bloggies activity

--- app/src/main/java/com/technifutur/evand2/MainActivity.kt
+++ app/src/main/java/com/technifutur/evand2/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.technifutur.evand2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.technifutur.evand2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.twitterButton.setOnClickListener {
+            val intent = Intent(this, TwitterLogin::class.java)
+            startActivity(intent)
+        }
+        binding.facebookButton.setOnClickListener {
+            val intent = Intent(this, FacebookLogin::class.java)
+            startActivity(intent)
+        }
+        binding.bloggiesButton.setOnClickListener {
+            val intent = Intent(this, SignupBloggies::class.java)
+            startActivity(intent)
+        }
     }
+
 }
